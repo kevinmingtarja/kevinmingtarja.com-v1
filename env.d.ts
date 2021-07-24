4ba3f98eb52ff792de245fda61ada84cf7854b3d
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2021-07-24 16:20:34 +0800

    Add prettier config and modify eslintrc

---
+++ env.d.ts
@@ -0,0 +1,19 @@
+declare module "*.scss" {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module "*.css" {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module "*.md" {
+  // eslint-disable-next-line one-var
+  const attributes: {
+    last_updated_time: string
+  }
+  // eslint-disable-next-line one-var
+  const html: string
+  export { attributes, html }
+}
