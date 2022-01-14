fd7c672dc03628db01a8f6306f5712cb56f85d4d
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2022-01-15 01:52:55 +0800

    Add tf configs for aws s3

---
+++ infra/s3.tf
@@ -0,0 +1,32 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket        = "kevinmingtarjacom-bucket"
+  acl           = "private"
+  force_destroy = true
+}
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.bucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"]
+    }
+  }
+
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = ["${aws_s3_bucket.bucket.arn}"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "s3_cloudfront_policy" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
