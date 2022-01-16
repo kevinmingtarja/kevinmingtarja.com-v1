54d531ad22e9087d22c6294c00fd56bc8022473a
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2022-01-17 01:33:15 +0800

    Add invalidation script and step to ci/cd flow

---
+++ .github/invalidate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Find all html files in the current directory
+arr=($(find . -name "*.html"))
+i=0
+
+# Remove trailing dot
+for file in "${arr[@]}"
+do
+    arr[$i]=${arr[$i]:1}
+    i=$((i + 1))
+done
+
+declare -p arr
+
+# Invalidate the html files
+aws cloudfront create-invalidation --distribution-id E26QI20L1ODVTP --paths "${arr[@]}"
